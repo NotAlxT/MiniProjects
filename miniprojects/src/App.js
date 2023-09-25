85e67b1571aba5d2e0a3f04ea42d2348df312233
NotAlxT <anhntran56@gmail.com>
2023-09-24 21:33:49 -0400

    Website outline
    created basic layout for files

---
+++ miniprojects/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './components/home/home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ miniprojects/src/components/home/home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './home.css'
+
+export default function Home() {
+    return (
+        <div>
+            <div className="homeContainer">
+                <section className="projectContainer">
+                    <div className="project">
+                        p1
+                    </div>
+
+                    <div className="project">
+                        p2
+                    </div>
+
+                    <div className="project">
+                        p3
+                    </div>
+
+                    <div className="project">
+                        p4
+                    </div>
+
+                    <div className="project">
+                        p5
+                    </div>
+
+                    <div className="project">
+                        p6
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
